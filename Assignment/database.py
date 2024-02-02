6cc1ee111de7504e306434c2623dbce9589e16c2
dhruvadity <cse.20201058@gmail.com>
2024-02-03 00:10:26 +0530

    modified exception handling

--- Assignment/database.py
+++ Assignment/database.py
@@ -6,12 +6,12 @@ URL_DATABASE = 'postgresql://postgres:Post123@localhost/Quiz App'
 engine=create_engine(URL_DATABASE)
 SessionLocal= sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
-# def initialize_db():
-#     db=SessionLocal()
-#     try:
-#         for quiz_data in quizzes_data:
-#             db_quiz=Quiz(**quiz_data)
-#             db.add(db_quiz)
-#         db.commit()
-#     finally:
-#         db.close()
+def initialize_db():
+    db=SessionLocal()
+    try:
+        for quiz_data in quizzes_data:
+            db_quiz=Quiz(**quiz_data)
+            db.add(db_quiz)
+        db.commit()
+    finally:
+        db.close()
--- Assignment/main.py
+++ Assignment/main.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+#Here the quiz is created in the local database which will be reflected to the user when call the GET Method
 quizzes_data=[
     {
         "title":"AI Quiz",
@@ -30,54 +31,64 @@ quizzes_data=[
         ]
     }
 ]
+#initialize function is called to initially show the questions on the UI
 initialize_db()
+#This is get method which has route /quizzes/id means it will show the specific quiz according to their id
 @app.get("/quizzes/{quiz_id}")
 async def get_quiz(quiz_id:int):
     db=SessionLocal()
     quiz=db.query(Quiz).filter(Quiz.id ==quiz_id).first()
     db.close()
+    #This  will check for NULL if there is no any quiz  then it will be handled by this if else statement
     if quiz is None:
         raise HTTPException(status_code=404,detail="Quiz Not found")
     return quiz
+#As we know submit is a post method because we are basically creating a new data. so method will be called
+#we can submit the answer according to the quiz_id
 
 @app.post("/submit/{quiz_id}")
 async def submit_quiz(quiz_id: int, answers: dict):
     db = SessionLocal()
     quiz = db.query(Quiz).filter(Quiz.id == quiz_id).first()
     db.close()
+    #Exception handling
     if quiz is None:
         raise HTTPException(status_code=404, detail="Quiz not found")
-
+    #initially user_score is declared 0
     user_score = 0
     correct_answers = []
-
+    #This for loop iterates through all the questions inside the quiz and also get the answer of the user, if the user have given correct answer then marks will be added
     for i, question in enumerate(quiz.questions):
         user_answer = answers.get(str(i + 1))
         if user_answer and user_answer.upper() == question["correct_answer"]:
             user_score += 1
             correct_answers.append(i + 1)
-
     result = {"user_score": user_score, "correct_answers": correct_answers}
     return result
 
 
-
+#class for choices of the question there is two attributes one is string and other one is whether this one is true or not
 class ChoiceBase(BaseModel):
     choice_text: str
     is_correct: bool
-
+#Question has two types of attributes one is string means question text and other one is list of options
 class QuestionBase(BaseModel):
     question_text: str
     choices: List[ChoiceBase]
+# declares a function named get_db that will serve as a dependency for database access.
 
 def get_db():
+    #creates a new database session using a SessionLocal object. This object is typically configured to manage sessions for a specific database connection pool.
     db = SessionLocal()
+    #initiates a try-finally block for exception handling.
     try:
         yield db
     finally:
+        #closes the database session, releasing resources and preventing potential connection leaks.
         db.close()
 
 db_dependency = Depends(get_db)
+#get method to get question by question id but this method is for those questions which is created by the help of post method not for the one which is already initialized in initial database
 @app.get("/questions/{question_id}")
 async def read_question(question_id: int, db: db_dependency):
     result = db.query(models.Questions).filter(models.Questions.id==question_id).first()
@@ -90,6 +101,7 @@ async def read_choices(question_id:int, db:db_dependency):
           if not result:
              raise HTTPException (status_code=404,detail='Question is not found')
           return result
+#Here post method is used to create question as well, there can be two ways to create question one can be predefined other one can be created by the help of post method
 
 @app.post("/questions")
 async def create_questions(question: QuestionBase, db: Session = db_dependency):
