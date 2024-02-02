71cdbce61db484f88c1bb629ff32477f30cd8566
dhruvadity <cse.20201058@gmail.com>
2024-02-02 23:44:05 +0530

    Completed the assignment

--- Assignment/main.py
+++ Assignment/main.py
@@ -1,38 +1,6 @@
-# from fastapi import FastAPI,HTTPException,Depends
-# from pydantic import BaseModel
-# from typing import List,Annotated
-# import models
-# from database import engine,SessionLocal
-# from sqlalchemy.orm import Session
-# app=FastAPI()
-# models.Base.metadata.create_all(bind=engine)
-# class ChoiceBase(BaseModel):
-#     choice_text=str
-#     is_correct=bool
-
-# class QuesionBase(BaseModel):
-#     question_text=str
-#     choices:List[ChoiceBase]
-# def get_db():
-#     db=SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-# db_dependency=Annotated[Session,Depends(get_db)]
-# @app.post("/questions")
-# async def create_questions(question:QuesionBase,db:db_dependency):
-#     db_question=model.Questions(question_text=question.question_text)
-#     db.add(db_question)
-#     db.commit()
-#     db.refresh in question.choices:
-#         db_choice =models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct,question_id=db_question.id)
-#         db.add(db_choice)
-#     db.commit()
-    
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-from typing import List
+from typing import List,Annotated
 import models
 from database import engine, SessionLocal, initialize_db
 from sqlalchemy.orm import Session
@@ -47,39 +15,50 @@ quizzes_data=[
                 "statement":"What is full form of AI?" ,
                 "options":["A. Artificial Intelligence", "B. Arduino Intelligence"],
                 "correct_answer":"A",
+            },
+            {
+                "statement":"What is the most interesting thing to learn in this decade?" ,
+                "options":["A. Artificial Intelligence", "B. Fashion design"],
+                "correct_answer":"A",
+            },
+            {
+                "statement":"What is statue of unity located" ,
+                "options":["A. Bihar", "B. Gujarat"],
+                "correct_answer":"B",
             }
+
         ]
     }
 ]
-# initialize_db()
-# @app.get("/quizzes/{quiz_id}")
-# async def get_quiz(quiz_id:int):
-#     db=SessionLocal()
-#     quiz=db.query(Quiz).filter(Quiz.id ==quiz_id).first()
-#     db.close()
-#     if quiz is None:
-#         raise HTTPException(status_code=404,detail="Quiz Not found")
-#     return quiz
+initialize_db()
+@app.get("/quizzes/{quiz_id}")
+async def get_quiz(quiz_id:int):
+    db=SessionLocal()
+    quiz=db.query(Quiz).filter(Quiz.id ==quiz_id).first()
+    db.close()
+    if quiz is None:
+        raise HTTPException(status_code=404,detail="Quiz Not found")
+    return quiz
 
-# @app.post("/submit/{quiz_id}")
-# async def submit_quiz(quiz_id: int, answers: dict):
-#     db = SessionLocal()
-#     quiz = db.query(Quiz).filter(Quiz.id == quiz_id).first()
-#     db.close()
-#     if quiz is None:
-#         raise HTTPException(status_code=404, detail="Quiz not found")
+@app.post("/submit/{quiz_id}")
+async def submit_quiz(quiz_id: int, answers: dict):
+    db = SessionLocal()
+    quiz = db.query(Quiz).filter(Quiz.id == quiz_id).first()
+    db.close()
+    if quiz is None:
+        raise HTTPException(status_code=404, detail="Quiz not found")
 
-#     user_score = 0
-#     correct_answers = []
+    user_score = 0
+    correct_answers = []
 
-#     for i, question in enumerate(quiz.questions):
-#         user_answer = answers.get(str(i + 1))
-#         if user_answer and user_answer.upper() == question["correct_answer"]:
-#             user_score += 1
-#             correct_answers.append(i + 1)
+    for i, question in enumerate(quiz.questions):
+        user_answer = answers.get(str(i + 1))
+        if user_answer and user_answer.upper() == question["correct_answer"]:
+            user_score += 1
+            correct_answers.append(i + 1)
 
-#     result = {"user_score": user_score, "correct_answers": correct_answers}
-#     return result
+    result = {"user_score": user_score, "correct_answers": correct_answers}
+    return result
 
 
 
--- Assignment/models.py
+++ Assignment/models.py
@@ -1,16 +1,3 @@
-# from sqlalchemy import Boolean, column, ForeignKey,Integer,String
-# from database import Base
-
-# class Questions(Base):
-#     __tablename__ ='questions'
-#     id=Column(Integer,primary_key=True,index=True)
-#     question_text=Column(String,index=True)
-# class Choices(Base):
-#     __tablename__ ='choices'
-#     id=Column(Integer,primary_key=True,index=True)
-#     choice_text=Column(String,index=True)
-#     is_correct= Column(Boolean,default=False)
-#     question_id=Column(Integer,ForeignKey("questions.id"))
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON
 from sqlalchemy.orm import relationship
 from database import Base
@@ -31,8 +18,8 @@ class Choices(Base):
     question_id = Column(Integer, ForeignKey("questions.id"))
     question = relationship("Questions", back_populates="choices")
 
-# class Quiz(Base):
-#     __tablename__=='quizzes'
-#     id=Column(Integer, primary_key= True, index=True)
-#     title=Column(String , index=True)
-#     questions=Column(JSON)
+class Quiz(Base):
+    __tablename__='quizzes'
+    id=Column(Integer, primary_key= True, index=True)
+    title=Column(String , index=True)
+    questions=Column(JSON)
